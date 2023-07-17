6f1738a9dbab90c76c90e6a114e745cd6f1b927e
Yeon-seong <dd300@naver.com>
2023-07-17 23:29:15 +0900

    객체로 프로퍼티를 가진 데이터 저장하기, 객체 내부 프로퍼티에 접근하기, 객체 업데이트 및 새로운 객체 프로퍼티 추가하기

--- js_momentum/app.js
+++ js_momentum/app.js
@@ -1,34 +1,31 @@
-/* 데이터를 리스트로 그룹화 : 데이터로 이루어진 배열 만들기 */
-// 배열의 목적은 하나의 변수 안에 데이터 리스트를 가지는 것
-const a = "apple";
-const b = "banana";
-const c = "cola";
-const d = "donut";
-const food = [a, b, c, d];
-console.log(food);
-console.log(food[1]);
+/* object(객체) */
+// 1. 중괄호 {}를 사용하며 변수명에 대한 property(특성)을 작성한다.
+// 2. 객체 안에서 선언된 이름과 값으로 이루어진 한 쌍
+// (property)를 가진 데이터를 저장하도록 해준다.
+// 3. 객체 안에서는 '='을 사용하지 않고 ':'을 사용한다.
+const player = {
+  name: "YeonSeong",
+  point: 100,
+  cute: true,
+};
 
 
-// nonsense 배열
-// 배열에는 숫자, 문자, 불린 값, null, undefined, 변수 등을 넣을 수 있다.
-const nonsense = [12, "hello", true, false, null, undefined];
-console.log(nonsense);
+/* 객체 내부 프로퍼티에 접근하는 방법 */
+// 점 표기법(dot notation)으로 player의 name에 접근하기
+console.log(player.name);
 
 
-// dayOfWeek 배열 : 7개의 엘리먼트로 이루어진 항목을 나열하는 배열
-// 컴퓨터는 0부터 숫자를 센다.
-// 인덱스는 0부터 시작한다.(0, 1, 2, 3, 4, 5)
-const dayOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+// 괄호 표기법(bracket notation)으로 player의 point에 접근하기
+console.log(player["point"]);
 
 
-/* 배열(array) 안에서 항목을 받아오기 */
-// 한 요일, 한 주의 몇 번째 날을 알고싶을 때 방법
-// dayOfWeek array에서 1번째 element를 알고 싶을 때
-// console.log(dayOfWeek[0]);
-console.log(dayOfWeek); // "sun" 추가 전, 엘리먼트(요소)가 6개
-
-
-/* 배열(array) 안에 요일 더 추가하기 */
-// 배열이름.push("추가 항목") : 항목 하나를 array 안에 추가하고 싶을 때 사용
-dayOfWeek.push("sun");  // dayOfWeek 배열 안에 "sun"을 추가
-console.log(dayOfWeek); // "sun" 추가 후, sun을 포함한 엘리먼트(요소)가 7개
+/* 객체 업데이트 및 새로운 객체 프로퍼티 추가하기 */
+// const는 let과 다르게 업데이트가 안되지만
+// 리스트의 경우 전체를 변경하는 게 아니라 속성값을 수정 or 추가하는 경우에는 업데이트가 가능하다.
+// property를 바꾸는 것은 가능하지만 선언된 object를 바꾸는 것은 불가능하다.
+console.log(player);  // player 객체의 프로퍼티 변경 전
+player.cute = false;  // player 객체의 프로퍼티 cute: true를 cute: false로 업데이트하기
+player.lastName = 'pororo';  // player 객체에 새로운 프로퍼티를 추가하기
+player.point = player.point + 400;   // player 객체의 프로퍼티 point에 400을 더하기
+console.log(player);  // player 객체의 프로퍼티 변경 후
+console.log(player.point);
