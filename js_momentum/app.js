86b43b9d35da15071c3a976527da10934393577b
Yeon-seong <dd300@naver.com>
2023-07-18 21:14:58 +0900

    데이터를 함수 안으로 보내고 보내진 데이터를 받는 방법 학습, 객체 안에서 매개변수가 인수를 얻는 방법 학습

--- js_momentum/app.js
+++ js_momentum/app.js
@@ -1,46 +1,78 @@
-/* function(함수) */
-// 함수는 계속 반복해서 사용할 수 있는 코드 조각이다.
-// 코드를 캡슐화해서, 실행을 여러 번 할 수 있게 한다.(코드의 반복↓)
+/* function(함수)에 데이터를 보낸 후 보내진 데이터를 받는 방법 학습하기 */
+// 인수를 보내고, function 함수명()의 괄호()에 데이터를 추가하면 데이터를 받을 수 있다.
+// 함수는 2개의 인수를 받을 수도 있다.
 
 
-/* 함수를 만드는 방법 */
-// funtion 함수명() {
-//   실행 코드 작성(중괄호{} 안에 작성하는 코드가 실행버튼을 누를 때마다 실행된다.)
-// }
+// sayHello로 보내진 첫 번째 데이터가 nameOfPrerson이라는 변수로 가게 된다.
+// 첫 번째 인수로 데이터가 들어오면 nameOfPrerson이라는 변수 이름을 쓴다.
+// nameOfPrerson과 age는 sayHello 함수의 body 안에서만 존재한다.
+function sayHello(nameOfPerson, age) {
+  console.log(
+    "안녕! 내 이름은 " + nameOfPerson + "고, " + age + "살이야. " + "만나서 반가워!"
+  );
+}
+
+/* 인수를 함수에 보내기 : 함수를 실행하면서 동시에 데이터를 보내기 */
+sayHello("Alice", 13);
+sayHello("Belua", 12);
+sayHello("Chole", 11);
+sayHello("Daisy", 10);
+
+// -------------------------------------------------------------------------------------
+
+/* 계산기 만들기 */
+// plus 함수는 firstNumber, secondNumber 두 개의 인수를 가진다.
+// 첫 번째 변수인 firstNumber에는 첫 번째 인수의 데이터 '10'이 전달되고,
+// 두 번째 변수인 secondNumber는 두 번째 인수인 2를 가져간다.
+function plus(firstNumber, secondNumber) {
+  console.log(firstNumber + secondNumber);
+}
+
+// firstNumber와 secondNumber는 plus 함수 안에서만 존재하며, 블록 밖에서 존재할 수 없다.
+// plus 함수 밖에서는 참조 오류(Uncaught ReferenceError)가 난다.
+// console.log(firstNumber + secondNumber);
 
 
-/* 함수의 실행버튼을 누르는 방법 */
-// console.log(), Array.push(), alert()처럼 괄호()가 함수를 실행하는 방법이다.
-// function 함수명()의 괄호가 실행버튼이다. 괄호()를 쓴 것이 실행버튼을 누른 것.
-
-
-/* 함수 괄호()를 비우기 */
-// 이전에 alert()를 사용할 때 ()안에 있는 정보를 함수에게 보냈었다.
-// 만약 괄호()를 비우고 alert()를 실행하면 내용 없이 비어있는 alert가 뜬다.
-alert();
-
-
-// log로 정보를 보내려면 console.log()의 괄호()안에 정보를 넣는다.
-// 실행버튼을 눌렀지만 아무런 정보도 보내지 않았기 때문에 아무것도 출력하지 않는다.
-console.log();
-
-
-/* 우리가 넘겨주는 어떠한 이름이든 Hello를 해주는 함수를 작성하기 */
-// sayHello의 실행을 누를 때마다 console.log("Hello! my name is C")가 실행된다.
-// Hello my name is C의 C에 다른 값(ex : 여러 사람 이름)을 쓰기 위해서는 인수를 보내야 한다.
-// argument(인수)는 함수를 실행하는 동안 어떤 정보를 함수에게 보낼 수 있는 방법을 말한다.
-function sayHello() {
-  console.log("Hello! my name is C");
+// a와 b도 divide 함수 안에서만 존재한다.
+function divide(a, b) {
+  console.log(a / b);
 }
 
 
-/* 함수를 실행하면서(함수 실행버튼을 누르면서) 동시에 데이터를 보내기 */
-// 함수를 실행 : 함수명();
-sayHello("Alice");  // 실행버튼을 누르고 "Alice"라는 데이터를 sayHello에 보내기
-sayHello("Bella");  // 실행버튼을 누르고 "Bella"라는 데이터를 sayHello에 보내기
-sayHello("Chloe");  // 실행버튼을 누르고 "Chloe"라는 데이터를 sayHello에 보내기
+// 실행 버튼을 눌러야 하기에 괄호()를 써야 한다.
+// plus 함수의 실행버튼 누르기
+plus(10, 2);
+
+// divide 함수의 실행버튼 누르기
+divide(33, 11);
 
 
-// 함수에 값이나 데이터를 보내는 방법을 학습했다.
-// console.log의 경우 console.log("Alice")을 그대로 인쇄하지만
-// sayHello 함수의 경우에는 "Hello! my name is"와 우리가 보낸 데이터를 출력해야 한다.
+// -------------------------------------------------------------------------------------
+
+/* 객체(object) 안에서 매개 변수가 인수를 얻는 방법 학습하기 */
+
+/* player object(객체) */
+// 문자, 숫자, boolean 같은 데이터 타입을 넣어주는 대신에 sayHello는 함수와 같다고 한다.
+// sayHello라는 player 객체 안의 함수를 부를 때
+const player = {
+  name: "YeonSeong",
+  sayHello: function(otherPersonName) {
+    console.log("hello! " + otherPersonName + " nice to meet you!");
+  },
+};
+
+
+// sayHello에게 바깥으로부터 인수를 받도록 한다.
+// sayHello 함수는 "Amand"라는 첫 번째 인수를 받고, 그게 otherPersonName으로 간다.
+// player.sayHello() 안에 값을 넣으면 console.log처럼 출력한다.
+console.log(player.name);
+player.sayHello("Amand");
+player.sayHello("Emily");
+
+
+/* 함수에게 정보를 보내고 받는 방법 */
+function heart(c, d) {  // (c, d)는 매개변수로 함수가 보내진 정보를 받는다.
+  console.log(c + d);
+}
+
+heart(5, 5);  // 데이터(인수)를 함수 안으로 보낸다.
