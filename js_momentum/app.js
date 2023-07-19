555d9345e772582e242a850dc612bcbc6ba77e10
Yeon-seong <dd300@naver.com>
2023-07-19 21:23:30 +0900

    대체하기(replacing)를 통해 함수 리턴의 흐름을 이해하기

--- js_momentum/app.js
+++ js_momentum/app.js
@@ -1,19 +1,16 @@
-/*  calculator 객체 안의 함수를 사용하여 사칙연산, 제곱를 계산하는 계산기 만들기
-    이전에는 console.log를 사용해 계산 결과를 콘솔에서 볼 수 있도록 만들었다.
-    하지만 console.log는 콘솔에 결과를 표시할 뿐, 함수로부터 결과를 제공받을 수는 없다.
-    함수 안에 console.log를 사용하지 않고 리턴(return)을 써서 작업의 결과를 알 수 있다. */
+/*  a+b 같은 실제적인 계산은 자바스크립트 코드에서 value(값)을 바로 얻을 수가 없다.
+    리턴 대신 console.log를 실행해도 잘 작동하는 것처럼 콘솔에 표시되지만
+    함수 외부에서 값을 제공받으려면 리턴을 사용해야 한다. */
 
-/*  리턴하는 것의 포인트는 함수 내부에서 console.log 하는 것이 아니라
-    (콘솔이 아닌) 화면에 결과값을 보여주는 것이다. */
+/*  나중에 사용자의 위치에 따라 날씨 정보를 알고 싶을 때
+    날씨 정보를 구한 다음. 그 값을 화면에 출력해야 한다.
+    그래서 console.log 대신에 리턴(return)을 사용할 예정이다. */
 
 // -------------------------------------------------------------------------------------
 
-/*  아래에 있는 변수들은 값이 있다. 결과 값을 리턴하면 그냥 console.log 했을 때와는 달리,
-    plusResult, miunsResult, multResult, dividResult, squaResult에 접근할 수 있다. */
-
 const calculator = {
   plus: function(a, b) {
-    return a + b;   // 더하기
+    return a + b;   // 더하기.
   },
   miuns: function(a, b) {
     return a - b;   // 빼기
@@ -25,25 +22,34 @@ const calculator = {
     return a / b;   // 나누기
   },
   squa: function(a, b) {
+    console.log("hello"); // 리턴을 하기 전에는 코드가 작동한다.
     return a ** b;  // 제곱
+    console.log("bye");   // 리턴하는 순간, 함수는 작동을 멈추고 결과값을 리턴하고 종료된다.
   }
 };
 
+/*  변수에 함수를 할당하면 plusResult 변수는 calculator 함수의 리턴 값을 가지게 된다.
+    만약 함수를 const, let, var 변수에 할당하면 그 변수는 plus 함수의 결과 타입을 가지게 된다.
 
-/*  연산결과 자체가 변수에게 주어지는 상호의존적인 코드를 만들기
-    계산기 안에서 값을 콘솔로 보내는 것이 아니라 리턴을 통해 함수를 호출한
-    변수에게 다시 주어서 그걸 갖고 또 다른 함수를 호출하는데 쓴다.
-    앞선 연산의 값이 뒤의 연산과 연관되고 체인처럼 서로 얽혀 연결된 코드를 만들었다.
-    이처럼 값의 대체를 할 수 있고, 변수 자체가 함수의 리턴 값 */
+    이 뜻은 calculator.plus가 5를 리턴하면 plusResult는 5가 된다. 이는 리턴했을 때에만 가능하다. */
+/*  리턴을 하지 않았을 때 plusResult의 결과값을 확인해보면 plusResult은 undefined다. */
+    const plusResult = calculator.plus(2, 3);
 
-const plusResult = calculator.plus(2, 3);
-const miunsResult = calculator.miuns(plusResult, 10);
-const multResult = calculator.mult(10, miunsResult);
-const dividResult = calculator.divid(multResult, plusResult);
-const squaResult = calculator.squa(dividResult, miunsResult);
 
-/*  calculator가 calculator.plus(2, 3) 작업의 결과를 준다.
-    plusResult에 결과를 가지고 있다.
-    plus 함수가 무언가를 리턴하기 때문에 이 함수를 호출하는 코드가 그 함수의 리턴 값이 된다.
-    plusResult는 calculator.plus(2, 3)의 결과랑 같다.
-    calculator.plus은 plus 함수에 2와 3을 보내고, 리턴 값은 5가 되어, plusResult는 5가 된다. */
+// -------------------------------------------------------------------------------------
+
+
+/*  대체하기(replacing)
+    1. 호출되는 함수를 보고 plusResult의 마지막에 저장되는 값을 알고 싶다면 plus 함수를 보자.
+    2. plus 함수는 a, b를 인자(매개변수)로 가지니까, a는 2, b는 3으로 대체된다.
+    3. 리턴하는 a + b는 2 + 3이 되고, 5를 리턴한다.
+    4. calculator.plus(2, 3)의 리턴 값이 5가 된다.
+    5. 그래서 console.log(plusResult);를 하면 5가 나온다.
+    6. 함수는 실행되고 나면 사라지고, 마지막에 결과를 남긴다. */
+
+// const calculator = {
+//   plus: function(2, 3) {
+//     return 2 + 3;   // 더하기. 즉, 5를 리턴한다.
+//   }
+// };
+// const plusResult = calculator.plus(2, 3);
