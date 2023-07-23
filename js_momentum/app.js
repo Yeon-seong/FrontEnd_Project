b3af825e4a0f8dfeae0b6a647f73fc0d4aecf3fb
Yeon-seong <dd300@naver.com>
2023-07-23 18:32:22 +0900

    HTML의 style을 자바스크립트에서 변경하기 CSS 선택자를 사용하여 조건에 맞는 첫 번째 노드를 찾고, 해당 글자색을 파란색으로 바꾸기

--- js_momentum/app.js
+++ js_momentum/app.js
@@ -1,15 +1,11 @@
-/*  자바스크립트에서 HTML 요소를 가져오고 검색하기  */
+/*  자바스크립트 객체 내부에 있는 속성들의 값을 변경하기  */
+
 
 
 /*  class hello를 가진 div 내부의 첫 번째 노드(first-child)인 h1을 찾아오기 */
-//  CSS 선택자(selector)를 전달할 수 있다.
-//  CSS 방식(.hello h1:first-child)으로 요소를 검색한다.
-const title = document.querySelector(".hello:first-child h1");
+const title = document.querySelector("div.hello:first-child h1");
 
-console.log(title);
 
-title.innerText = "Hello";
-
-/*  h1 요소의 내부를 보고 싶을 때 사용 */
-//  기본적으로 객체로 표시한 요소를 보여준다.
-console.dir(title);
+/*  h1의 style을 자바스크립트에서 변경하기 */
+//  hello라는 class를 가진 첫 번째 h1의 글자색을 파란색으로 바꾸기
+title.style.color = "blue";
