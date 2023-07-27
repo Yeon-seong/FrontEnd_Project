0564aa927722621e0cdab066a765fc18c9e88680
Yeon-seong <dd300@naver.com>
2023-07-27 16:45:21 +0900

    로그인 - input 사용자 이름 유효성 검사하기 login input을 필수입력 항목으로 만들고, 최대 입력 글자를 15로 조절하기

--- js_momentum/app.js
+++ js_momentum/app.js
@@ -5,11 +5,10 @@ const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 
 
-/*  사용자가 loginButton 클릭할 때 감지하는 함수 실행하기
-    사용자가 loginInput에 값(value)을 입력하고, loginButton을 누르면 콘솔에 값이 출력된다.  */
+/*  사용자가 loginButton을 클릭할 때 감지하는 함수 실행하기 */
 function onLoginClick() {
-  console.log(loginInput.value);
-  console.log("이름을 입력했습니다!");
+  const userName = loginInput.value;
+  console.log(userName);
 }
 
 
