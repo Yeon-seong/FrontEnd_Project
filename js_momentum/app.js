e13445f36c18337d196a3458c8c8a3d0ae7260e2
Yeon-seong <dd300@naver.com>
2023-07-28 07:49:14 +0900

    로그인 - preventDefault() 이해하기 a 태그를 클릭했을 때 href 링크로 이동하지 않도록 브라우저 기본 동작 막기

--- js_momentum/app.js
+++ js_momentum/app.js
@@ -4,6 +4,9 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
 
+const link = document.querySelector("a");
+
+
 /*  form의 제출/전송 이벤트(submit event)를 감지하는 함수 실행하기
     preventDefault 함수를 호출해, 폼을 submit 할 때 새로고침 되는 브라우저 기본 동작 막기  */
 function onLoginSubmit(event) {
@@ -11,6 +14,15 @@ function onLoginSubmit(event) {
   console.log(loginInput.value);
 }
 
+/*  preventDefault 함수를 호출해, a 태그를 cilck 했을 때 
+    href 링크로 이동하지 않게 브라우저 기본 동작 막기  */
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+  alert("클릭했습니다!");
+}
+
 
 /*  서브밋 이벤트가 발생한다면 onLoginSubmit 함수를 실행시킨다.  */
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
