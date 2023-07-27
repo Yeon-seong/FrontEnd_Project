4f1ef389aa1d9e7d139ff1e2cf2f09c7dfe8b6e3
Yeon-seong <dd300@naver.com>
2023-07-28 06:00:56 +0900

    로그인 - form을 서버에 제출할 때 새로고침 막기 preventDefault 함수를 호출해 브라우저 기본 동작 막기

--- js_momentum/app.js
+++ js_momentum/app.js
@@ -1,16 +1,16 @@
 /*  로그인 폼
-    HTML 요소인 login-form id를 가져오고, login-form id 안에 있는 input과 button을 찾기 */
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+    HTML 요소인 login-form id와 login-form id 안에 있는 input을 찾아 가져오기 */
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
 
-/*  사용자가 loginButton을 클릭할 때 감지하는 함수 실행하기 */
-function onLoginClick() {
-  const userName = loginInput.value;
-  console.log(userName);
+/*  form의 제출/전송 이벤트(submit event)를 감지하는 함수 실행하기
+    preventDefault 함수를 호출해, 폼을 submit 할 때 새로고침 되는 브라우저 기본 동작 막기  */
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(loginInput.value);
 }
 
 
-/*  클릭 이벤트를 loginButton에 연결해, onLoginClick 함수가 click을 감지하도록 만들기  */
-loginButton.addEventListener("click", onLoginClick);
+/*  서브밋 이벤트가 발생한다면 onLoginSubmit 함수를 실행시킨다.  */
+loginForm.addEventListener("submit", onLoginSubmit);
