c4051ce1249748ba99addc877e128fa9bcf817a0
Yeon-seong <dd300@naver.com>
2023-07-30 23:51:27 +0900

    시계 만들기 - 시, 분, 초를 두 자릿수로 출력하기 padStart 함수 사용. 문자열을 2로 늘리고, 앞쪽에 0을 추가하기

--- js_momentum/js/clock.js
+++ js_momentum/js/clock.js
@@ -2,10 +2,14 @@
 const clock = document.querySelector("h2#clock"); // h2와 clock id를 함께 가져오기
 
 
-/*  new Date 객체로 현재 날짜, 시간, 분, 초에 대한 정보를 가져와 새로운 Date 객체를 만든다.  */
+/*  new Date 객체로 현재 날짜, 시간, 분, 초에 대한 정보를 가져와 새로운 Date 객체를 만든다.
+    padStart() 함수를 사용해 앞쪽에 0을 추가하기  */
 function getClock() {
   const date = new Date();
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 
