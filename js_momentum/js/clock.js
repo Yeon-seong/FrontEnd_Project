42a1f7cc03fb5100779aa465c2dc73713346a989
Yeon-seong <dd300@naver.com>
2023-07-31 19:09:45 +0900

    Momentum App 시계 완성 현재 시각을 오전/오후로 표시하기

--- js_momentum/js/clock.js
+++ js_momentum/js/clock.js
@@ -2,14 +2,10 @@
 const clock = document.querySelector("h2#clock"); // h2와 clock id를 함께 가져오기
 
 
-/*  new Date 객체로 현재 날짜, 시간, 분, 초에 대한 정보를 가져와 새로운 Date 객체를 만든다.
-    padStart() 함수를 사용해 앞쪽에 0을 추가하기  */
+/*  new Date 객체로 현재 날짜, 시간, 분, 초에 대한 정보를 가져와 새로운 Date 객체를 만들기
+    toLocaleTimeString() 메서드를 사용해 현재 시각을 오전/오후로 표시하기  */
 function getClock() {
-  const date = new Date();
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  clock.innerText = new Date().toLocaleTimeString("ko-KR", { hour12: true });
 }
 
 
