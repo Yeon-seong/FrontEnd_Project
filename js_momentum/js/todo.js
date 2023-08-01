4bc0e277d19fec9583877fa6376496c031d8bbf8
Yeon-seong <dd300@naver.com>
2023-08-01 18:00:01 +0900

    ToDo List - toDo 삭제하기

--- js_momentum/js/todo.js
+++ js_momentum/js/todo.js
@@ -7,15 +7,25 @@ const toDoInput = toDoForm.querySelector("input");
 
 
 
-/*  paintToDo() 함수 : toDo를 그리기  */
-function paintToDo(newTodo) {                   // newTodo라는 인자를 주기
-  const li = document.createElement("li");      // html 내에 li태그를 생성
-  const span = document.createElement("span");  // html 내에 span태그를 생성
-  li.appendChild(span);                         // li 태그 안에 span이라는 자식을 추가
-  span.innerText = newTodo; //  handleToDoSubmit에서 온 사용자가 form에서 준 newTodo 값이 span의 텍스트가 됨
-  toDoList.appendChild(li); // 새로운 li를 리스트(toDoList)에 추가 : html의 ul 태그 안에 li를 속하게 함
+/*  deleteToDo : toDo를 삭제하는 함수  */
+function deleteToDo (event) {
+ const li = event.target.parentElement; // 삭제하고 싶은 li
+ li.remove();                           // button의 부모인 li를 제거.
 }
 
+
+/*  paintToDo : toDo를 그리는 함수  */
+function paintToDo(newTodo) {                       // newTodo라는 인자를 주기
+  const li = document.createElement("li");          // html 내에 li태그를 생성
+  const span = document.createElement("span");      // html 내에 span태그를 생성
+  const button = document.createElement("button");  // html 내에 button태그를 생성
+  span.innerText = newTodo;   // span의 텍스트를 사용자가 form에서 준 newTodo 값으로 변경
+  button.innerText = "삭제";  // button의 텍스트를 "삭제"로 변경
+  button.addEventListener("click", deleteToDo); // 에서 클릭이 발생하면 deleteToDo 함수 실행
+  li.appendChild(span);       // span이라는 자식을 li 태그 안에 추가 
+  li.appendChild(button);     // button이라는 자식을 li 태그 안에 추가
+  toDoList.appendChild(li);   // 새로운 li를 리스트(toDoList)에 추가
+}
 
 
 /*  handleToDoSubmit() 함수
@@ -28,6 +38,5 @@ function handleToDoSubmit(event) {  // newTodo : input의 value를 비우기 전
 }
 
 
-
-/*  handleToDoSubmit에 submit 이벤트가 발생했을 경우  */
+/*  handleToDoSubmit에 submit event가 발생했을 경우  */
 toDoForm.addEventListener("submit", handleToDoSubmit)
