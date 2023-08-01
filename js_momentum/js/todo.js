4e3031e46b5841b9cd48230079714666a8605992
Yeon-seong <dd300@naver.com>
2023-08-01 19:15:35 +0900

    ToDo List - toDo 저장하기 로컬 스토리지에 toDo를 저장해, 새로고침 해도 값을 유지하기 JSON.stringify 메서드를 사용해, 값의 배열을 문자열로 바꾸기

--- js_momentum/js/todo.js
+++ js_momentum/js/todo.js
@@ -2,9 +2,20 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 
-/*  document가 아닌 toDoform 안에서 input을 찾기  */
+/*  HTML 요소 가져오기 : document가 아닌 toDoform 안에서 input을 찾기  */
 const toDoInput = toDoForm.querySelector("input");
 
+
+
+/*  toDos : toDo에 들어오는 텍스트를 배열로 묶어 보관 */
+const toDos = [];
+
+
+/*  saveToDos : toDo 배열의 내용을 로컬 스토리지(localStorage)에 넣어 저장하는 함수
+    로컬 스토리지는 문자열만 저장 가능, JSON.stringify 메서드로 toDos 배열을 문자로 바꾸기  */
+function saveToDos() {
+  localStorage.setItem("toDos", JSON.stringify(toDos));
+}
 
 
 /*  deleteToDo : toDo를 삭제하는 함수  */
@@ -31,10 +42,12 @@ function paintToDo(newTodo) {                       // newTodo라는 인자를 �
 /*  handleToDoSubmit() 함수
     enter를 누르면 toDoInput의 모든 입력값이 사라지도록 toDoInput의 값에 빈값("")을 넣기  */
 function handleToDoSubmit(event) {  // newTodo : input의 value를 비우기 전 값을 나타내는 문자.
-  event.preventDefault();           // 폼 제출 시 새로고침을 막음
+  event.preventDefault();           // 사용자가 폼 제출 시 새로고침을 막음
   const newTodo = toDoInput.value;  // 제출하는 텍스트를 저장. input에서 value를 얻음
-  toDoInput.value = "";             // 엔터를 누르면 텍스트 상자 안의 텍스트를 빈값으로 초기화
-  paintToDo(newTodo);               // value를 얻어서 paintToDo 함수에 newTodo를 인자로 보냄
+  toDoInput.value = "";             // 사용자가 폼 제출 시 input 안의 텍스트를 빈값으로 초기화
+  toDos.push(newTodo);              // newTodo를 toDos 배열에 push하기
+  paintToDo(newTodo);               // paintToDo에 newTodo를 인자로 보내 화면에 toDo를 그림
+  saveToDos(toDos);                 // toDo들을 저장하기
 }
 
 
