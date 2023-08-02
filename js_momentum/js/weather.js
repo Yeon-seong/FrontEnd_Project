15ff614b7094428b552a7577cbf22e6e0b81e717
Yeon-seong <dd300@naver.com>
2023-08-03 07:36:45 +0900

    Momentum App - Weather API 완성 사용자 위치에 따른 도시 이름, 날씨, 온도를 알아내기

--- js_momentum/js/weather.js
+++ js_momentum/js/weather.js
@@ -1,8 +1,20 @@
-/*  사용자 위치(geolocation) 알아내기  */
+/*  HTML 요소 가져오기  */
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "66f43e92393509a81ee850a9885aafbe";
+
+
+/*  사용자 위치(geolocation)와 날씨(weather) 알아내기  */
 function onGeoOk(position) {
   const lat = position.coords.latitude;		// 위도(latitude)
-  const lng = position.coords.longitude;	// 경도(longitude)
-  console.log("당신은 잘 지내고 있군요!", lat, lng);
+  const lon = position.coords.longitude;	// 경도(longitude)
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      city.innerText = data.name; // 사용자 위치의 도시 이름
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;  // 날씨이름 / 온도
+    });
 }
 function onGeoError() {
   alert("날씨 위치를 찾을 수 없습니다.");
