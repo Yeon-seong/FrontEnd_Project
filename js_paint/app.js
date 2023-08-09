3dd07e71a682165bbc56e60025b1321f42415e13
Yeon-seong <dd300@naver.com>
2023-08-09 09:14:33 +0900

    paint - 사람 만들기 canvas arc 메서드 사용

--- js_paint/app.js
+++ js_paint/app.js
@@ -7,29 +7,46 @@ const ctx = canvas.getContext("2d");
 
 
 /*  JS의 캔버스 : 너비(가로)와 높이(세로)는 캔버스의 해상도 설정  */
-canvas.width = 600;
-canvas.height = 600;
+canvas.width = 700;
+canvas.height = 700;
 
 
-/*  캔버스로 집 만들기  */
+/*  사람 만들기
+    canvas arc() 메소드 사용 : 캔버스에서 원 그리기  */
 
-//  벽
-ctx.fillStyle = "#30A2FF";
-ctx.fillRect(150, 250, 50, 200);   // 왼쪽 벽 x: 150, y: 250, 너비: 50, 높이: 200
-ctx.fillRect(400, 250, 50, 200);   // 오른쪽 벽 x: 400, y: 200, 너비: 50, 높이: 200
-ctx.lineWidth = 5;
+//  색상
+ctx.fillStyle = "#0099FF";
 
 
-//  문
-ctx.fillRect(250, 350, 100, 100);  // x: 250, y: 350, 너비: 100, 높이: 100
+//  팔
+
+ctx.fillRect(220, 250, 90, 20);
+ctx.fillRect(390, 250, 90, 20);
 
 
-//  천장
-ctx.fillRect(200, 250, 200, 50)    // x: 200, y: 250, 너비: 200, 높이: 50
+//  몸통
+ctx.fillRect(310, 250, 80, 150);
 
 
-//  지붕
-ctx.moveTo(150, 250);
-ctx.lineTo(300, 130);
-ctx.lineTo(450, 250);
+//  다리
+ctx.fillRect(310, 400, 20, 100);
+ctx.fillRect(370, 400, 20, 100);
+
+
+//  머리
+ctx.arc(350, 200, 50, 0, 2 * Math.PI);
+ctx.fill();
+
+
+//  눈
+ctx.beginPath();
+ctx.fillStyle = "#FFFFFF";
+ctx.arc(330, 200, 7, 0, 2 * Math.PI);
+ctx.arc(370, 200, 7, 0, 2 * Math.PI);
+ctx.fill();
+
+
+// 입
+ctx.beginPath();
+ctx.arc(350, 220, 10, 0, 1 * Math.PI);
 ctx.fill();
