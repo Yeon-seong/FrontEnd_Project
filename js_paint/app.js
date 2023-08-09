92d297ba5d5cacf0c55a8b3da1e7546fdf2fd6d9
Yeon-seong <dd300@naver.com>
2023-08-10 00:31:45 +0900

    canvas - 보드를 클릭할 때마다 선 그리기

--- js_paint/app.js
+++ js_paint/app.js
@@ -9,44 +9,16 @@ const ctx = canvas.getContext("2d");
 /*  JS의 캔버스 : 너비(가로)와 높이(세로)는 캔버스의 해상도 설정  */
 canvas.width = 700;
 canvas.height = 700;
+ctx.lineWidth = 2;
 
 
-/*  사람 만들기
-    canvas arc() 메소드 사용 : 캔버스에서 원 그리기  */
+/*	그림판 프로토타입 만들기
+		마우스로 보드를 클릭할 때마다 선 그리기	*/
 
-//  색상
-ctx.fillStyle = "#0099FF";
+function onClick(event) {
+	ctx.moveTo(0, 0);
+	ctx.lineTo(event.offsetX, event.offsetY);
+	ctx.stroke();
+}
 
-
-//  팔
-
-ctx.fillRect(220, 250, 90, 20);
-ctx.fillRect(390, 250, 90, 20);
-
-
-//  몸통
-ctx.fillRect(310, 250, 80, 150);
-
-
-//  다리
-ctx.fillRect(310, 400, 20, 100);
-ctx.fillRect(370, 400, 20, 100);
-
-
-//  머리
-ctx.arc(350, 200, 50, 0, 2 * Math.PI);
-ctx.fill();
-
-
-//  눈
-ctx.beginPath();
-ctx.fillStyle = "#FFFFFF";
-ctx.arc(330, 200, 7, 0, 2 * Math.PI);
-ctx.arc(370, 200, 7, 0, 2 * Math.PI);
-ctx.fill();
-
-
-// 입
-ctx.beginPath();
-ctx.arc(350, 220, 10, 0, 1 * Math.PI);
-ctx.fill();
+canvas.addEventListener("click", onClick)
