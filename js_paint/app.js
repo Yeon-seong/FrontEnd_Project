e283078b6452bf0eb6b70d5783de335500cb4d45
Yeon-seong <dd300@naver.com>
2023-08-12 03:26:40 +0900

    painting board - 선 굵기 수정하기 완성

--- js_paint/app.js
+++ js_paint/app.js
@@ -1,5 +1,9 @@
+//  ------------------------- 그림판 만들기 -------------------------
+
+
 /*  HTML 요소 가져오기  */
 const canvas = document.querySelector("canvas");
+const lineWidth = document.getElementById("line-width");
 
 
 /*  getContext() 메서드 호출  */
@@ -11,16 +15,18 @@ canvas.width = 1000;
 canvas.height = 1000;
 
 
+/*  true/false로 사용자가 그림을 그리는지 확인하기  */
+let isPainting = false;
 
-/*  그림판 만들기
-    캔버스 위에 마우스를 올리면 사용자의 마우스가 있는 곳으로 브러시를 움직이기
-    마우스를 누르고 움직일 때 사용자가 있던 곳부터 움직이는 곳으로 선을 그리기  */
 
-ctx.lineWidth = 2;      // 선 굵기
-let isPainting = false; // true/false로 사용자가 그림을 그리는지 확인
+/*  HTML의 input 초기 값(value)을 5로 주기  */
+ctx.lineWidth = lineWidth.value;
+
 
 
 /*  마우스를 움직였을 때(onMove)
+    캔버스 위에 마우스를 올리면 사용자의 마우스가 있는 곳으로 브러시를 움직이기
+    마우스를 누르고 움직일 때 사용자가 있던 곳부터 움직이는 곳으로 선을 그리기
     isPainting이 true면 마우스가 있는 쪽으로 선을 그려 함수를 끝내고,
     isPainting이 false면 브러시를 마우스가 있는 쪽으로 움직인다.	*/
 function onMove(event) {
@@ -34,19 +40,31 @@ function onMove(event) {
 
 
 /*  마우스를 누르고 있을 때(startPainting)  */
-function startPainting(event) {
+function startPainting() {
   isPainting = true;
 };
 
 
-/*  마우스를 뗐을 때(cancelPainting)  */
-function cancelPainting(event) {
+/*  마우스를 뗐을 때(cancelPainting)
+    사용자가 페인팅을 마치면 새로운 path를 만들기  */
+function cancelPainting() {
   isPainting = false;
+  ctx.beginPath();
 };
 
 
-/*  마우스 이벤트 추가  */
+/*  사용자의 입력 값에 따른 선 굵기  */
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+
+
+/*  마우스 이벤트 리스너  */
 canvas.addEventListener("mousemove", onMove);           // 마우스를 움직이면 onMove 함수 호출
 canvas.addEventListener("mousedown", startPainting);    // 마우스를 누르고 있으면 startPainting 함수 호출
 canvas.addEventListener("mouseup", cancelPainting);     // 마우스를 떼면 cancelPainting 함수 호출
 canvas.addEventListener("mouseleave", cancelPainting);  // 마우스가 캔버스를 떠나면 cancelPainting 함수 호출
+
+
+/*  선 굵기 이벤트 리스너  */
+lineWidth.addEventListener("change", onLineWidthChange);// 사용자가 입력 값을 바꿀 때 선 굵기 바꾸기
