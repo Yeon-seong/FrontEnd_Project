03351ef0d791c2c9308177e6fc24a548246fd8f3
Yeon-seong <dd300@naver.com>
2023-08-11 11:46:28 +0900

    painting board - 마우스 이벤트로 선 그리기 완성

--- js_paint/app.js
+++ js_paint/app.js
@@ -7,38 +7,46 @@ const ctx = canvas.getContext("2d");
 
 
 /*  JS의 캔버스 : 너비(가로)와 높이(세로)는 캔버스의 해상도 설정  */
-canvas.width = 700;
-canvas.height = 700;
-ctx.lineWidth = 2;
+canvas.width = 1000;
+canvas.height = 1000;
 
 
-/*	그림판 프로토타입 만들기
-		선을 그릴 때마다 색을 다르게 만들기	*/
 
-		// 색 배열
-		const colors = [
-			"#ff3838",
-			"#ffb8b8",
-			"#c56cf0",
-			"#ff9f1a",
-			"#fff200",
-			"#32ff7e",
-			"#7efff5",
-			"#18dcff",
-			"#7d5fff",
-		];
-		
+/*  그림판 만들기
+    캔버스 위에 마우스를 올리면 사용자의 마우스가 있는 곳으로 브러시를 움직이기
+    마우스를 누르고 움직일 때 사용자가 있던 곳부터 움직이는 곳으로 선을 그리기  */
 
-		/*	무작위로 색을 하나 가져오기
-				Math.random(), Math.floor() 사용	*/
-		function onClick(event) {
-			ctx.beginPath();
-			ctx.moveTo(350, 350);
-			const color = colors[Math.floor(Math.random() * colors.length)];
-		
-			ctx.strokeStyle = color;
-			ctx.lineTo(event.offsetX, event.offsetY);
-			ctx.stroke();
-		}
-		
-		canvas.addEventListener("mousemove", onClick)
+ctx.lineWidth = 2;      // 선 굵기
+let isPainting = false; // true/false로 사용자가 그림을 그리는지 확인
+
+
+/*  마우스를 움직였을 때(onMove)
+    isPainting이 true면 마우스가 있는 쪽으로 선을 그려 함수를 끝내고,
+    isPainting이 false면 브러시를 마우스가 있는 쪽으로 움직인다.	*/
+function onMove(event) {
+  if(isPainting) {		
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
+};
+
+
+/*  마우스를 누르고 있을 때(startPainting)  */
+function startPainting(event) {
+  isPainting = true;
+};
+
+
+/*  마우스를 뗐을 때(cancelPainting)  */
+function cancelPainting(event) {
+  isPainting = false;
+};
+
+
+/*  마우스 이벤트 추가  */
+canvas.addEventListener("mousemove", onMove);           // 마우스를 움직이면 onMove 함수 호출
+canvas.addEventListener("mousedown", startPainting);    // 마우스를 누르고 있으면 startPainting 함수 호출
+canvas.addEventListener("mouseup", cancelPainting);     // 마우스를 떼면 cancelPainting 함수 호출
+canvas.addEventListener("mouseleave", cancelPainting);  // 마우스가 캔버스를 떠나면 cancelPainting 함수 호출
